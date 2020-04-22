a4d29baadae5399bd9cd082eada193b6e3ed356e
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2020-04-22 14:34:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 // Write your code here!
 main.remove();
-let H1 = document.createElement("h1#victory");
+let h1 = document.createElement('h1#victory');
 document.body.appendChild(h1);
-let newHeader = document.querySelector(H1);
-newHeader.innerHTML = "Evan is the champion";
