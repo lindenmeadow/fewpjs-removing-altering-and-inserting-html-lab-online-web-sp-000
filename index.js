8afea635a8c53a291f3ff0645f035a645b254dcc
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2020-04-22 15:48:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Write your code here!
 main.remove();
 let newHeader = document.querySelector('h1#victory');
-newHeader.innerHTML = "YOUR-NAME are the champion";
+newHeader.innerHTML = "YOUR-NAME is the champion";
